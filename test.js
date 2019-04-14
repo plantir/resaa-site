713cacdaedcd8f9c23b33a377572e28c68c2aebb
armin <arminkheirkhahan@gmail.com>
2019-04-14 16:57:48 +0430

    remove test and

--- test.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios')
-let api = 'https://webapi.resaa.net/charge/denominations'
-api = 'http://resa-web-api.bsn.local/charge/denominations'
-axios.get(api).then(res => {
-    console.log(res.data.result.denominations);
-})
