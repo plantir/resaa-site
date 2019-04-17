77c067302369fc54af0250e33975ff04eef724d6
armin <arminkheirkhahan@gmail.com>
2019-04-18 00:18:08 +0430

    add api rewrite

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,9 @@ export default {
     port: port
   },
   proxy: {
-    '/api/': { target: process.env.NODE_ENV == 'development' ? 'https://webapi.resaa.net' : 'http://resa-web-api.bsn.local', pathRewrite: { '^/api/': '' } }
+    '/api/': { target: process.env.NODE_ENV == 'development' ? 'https://webapi.resaa.net' : 'http://resa-web-api.bsn.local', pathRewrite: { '^/api/': '' } },
+    '/api/mobile': { target: process.env.NODE_ENV == 'development' ? 'https://webapi.resaa.net' : 'http://api.bsn.local' },
+    '/api/messagecallback': { target: process.env.NODE_ENV == 'development' ? 'https://webapi.resaa.net' : 'http://api.bsn.local' },
   },
   serverMiddleware: [
     {
