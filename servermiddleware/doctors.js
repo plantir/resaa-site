fa41022479dfceae0e4c78f51cc01f5c9b6fa988
armin <arminkheirkhahan@gmail.com>
2020-02-04 16:37:36 +0330

    fix redirect doctors with trycatch

--- servermiddleware/doctors.js
+++ servermiddleware/doctors.js
@@ -10,15 +10,17 @@ module.exports = async function(req, res, next) {
   if (is_doctor) {
     let regex_result = regex.exec(req.url);
     let doctor_id = regex_result[2];
-    let { data } = await axios.get(`${API_URL}/doctors/${doctor_id}/profile`);
-    let doctor = data.result.doctor;
-    if (doctor) {
-      let url = `/doctors/${doctor.specialtyEnglishTitle
-        .toLowerCase()
-        .replace(/ /g, "-")}/${doctor.subscriberNumber}`;
-      res.writeHead(301, { Location: url });
-      return res.end();
-    }
+    try {
+      let { data } = await axios.get(`${API_URL}/doctors/${doctor_id}/profile`);
+      let doctor = data.result.doctor;
+      if (doctor) {
+        let url = `/doctors/${doctor.specialtyEnglishTitle
+          .toLowerCase()
+          .replace(/ /g, "-")}/${doctor.subscriberNumber}`;
+        res.writeHead(301, { Location: url });
+        return res.end();
+      }
+    } catch (error) {}
     // let
   }
   next();
